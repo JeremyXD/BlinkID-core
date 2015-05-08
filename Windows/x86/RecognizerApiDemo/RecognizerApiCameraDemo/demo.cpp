e8fde8966d4412e59bf31898e8f6827903dacee6
Zivac <ivan.zivkovic@racuni.hr>
2015-05-08 16:07:22 +0200

    [RecognizerApiDemo] bug with onShowImage fixed

--- Windows/x86/RecognizerApiDemo/RecognizerApiCameraDemo/demo.cpp
+++ Windows/x86/RecognizerApiDemo/RecognizerApiCameraDemo/demo.cpp
@@ -87,6 +87,8 @@ RecognizerCallback buildRecognizerCallback() {
 	cb.onProgress = NULL;
 	/* onShouldStopRecognition is called multiple times from some recognizers to check if recognition should be canceled. */
 	cb.onShouldStopRecognition = NULL;
+	/* onShowImage is called during recognition process and allows for additional image processing */
+	cb.onShowImage = NULL;
 	return cb;
 }
 
@@ -187,7 +189,7 @@ int main(int argc, char** argv)
 	recognizerSettingsSetMRTDSettings(settings, &mrtdSettings);
 
 	/* insert license key and licensee */
-	recognizerSettingsSetLicenseKey(settings, "Add licensee here", "Add license key here");
+	recognizerSettingsSetLicenseKey(settings, "Add licensee here", "Add license key here");	
 
 	/* create global recognizer with settings */
 	status = recognizerCreate(&recognizer, settings);
